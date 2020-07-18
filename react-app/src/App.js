6b217a53a75966324cb5b7b6e35b6d0b5ff84f6f
Japneet Singh <singhjapneetssa@gmail.com>
2020-07-19 01:40:46 +0530

    Completed left info pane

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,12 +5,26 @@ import markers from './markers';
 import './App.css';
 
 import img1 from './assets/img1.jpeg';
+import plastic_patch from './assets/plastic_patch.jpg';
+import microplastics from './assets/microplastics.jpg';
+import albatross from './assets/albatross.jpg';
+import plastic_debris from './assets/plastic_debris.jpg';
+import bathroom from './assets/bathroom.png';
+import bamboo from './assets/bamboo.png';
+import reef_safe from './assets/reef_safe.jpg';
+import shampoo_bars from './assets/shampoo_bars.jpg';
+import clean_seas from './assets/clean_seas.svg';
+import nat_geo from './assets/nat_geo.svg';
+import plogging from './assets/plogging.jpg';
+import ocean_cleanup from './assets/ocean_cleanup.png';
 
 function getTooltipContent(marker) {
+
   return `CITY: ${marker.city} (Value: ${marker.value})`;
 }
 
 function App() {
+  const [page, setPage] = useState(1);
   const [event, setEvent] = useState(null);
   const [details, setDetails] = useState(null);
   function onClickMarker(marker, markerObject, event) {
@@ -32,6 +46,7 @@ function App() {
   }
 
   return (
+    <>
     <div style={{ width: '100vw', height: '100vh' }}>
       <ReactGlobe
         markers={markers}
@@ -40,9 +55,9 @@ function App() {
         }}
         onClickMarker={onClickMarker}
         onDefocus={onDefocus}
+        style={{position: 'static'}}
       />
       {details && (
-        <>
         <div
           style={{
             background: 'rgba(0,0,0,0.6)',
@@ -101,34 +116,160 @@ function App() {
         }
         </div>
 
+        
+      )}
+      <button style={{height: 30, width: 30, borderRadius: 30, background: 'tomato', margin: 20, border: 'none', fontFamily: 'Chelsea Market', position: 'absolute', fontSize: 20, top: 0, left: 20, color: 'white',}} onClick={() => setPage(1)}>1</button>
+      <button style={{height: 30, width: 30, borderRadius: 30, background: 'orange', margin: 20, border: 'none', fontFamily: 'Chelsea Market', position: 'absolute', fontSize: 20, top: 0, left: 70, color: 'white'}} onClick={() => setPage(2)}>2</button>
+      <button style={{height: 30, width: 30, borderRadius: 30, background: 'mediumseagreen', margin: 20, border: 'none', fontFamily: 'Chelsea Market', position: 'absolute', fontSize: 20, top: 0, left: 120, color: 'white'}} onClick={() => setPage(3)}>3</button>
+
+      {(page===1) && (
         <div
           style={{
             background: 'rgba(0,0,0,0.6)',
             fontFamily: 'Chelsea Market',
             position: 'absolute',
             fontSize: 20,
-            top: 20,
+            top: 70,
             left: 20,
             width: 450,
             padding: 12,
             color: 'white',
-            borderRadius: 20
+            borderRadius: 20,
+            overflow: 'auto',
+            height: '90vh'
           }}>
           <button style={{
-            background: 'rgba(20,20,20,0.6)',
+            background: 'rgba(255,20,20,0.6)',
             fontFamily: 'Chelsea Market',
             fontSize: 20,
-            top: 20,
+            top: 0,
             right: 20,
             width: '100%',
             padding: 12,
             color: 'white',
             borderRadius: 14,
-            border: 0
-          }}>Some facts you ought to know!</button> 
+            border: 0,
+            position:'sticky',
+          }}>THE GREAT PACIFIC GARBAGE PATCH</button> 
           <br />
-          <p style={{textAlign: 'center'}}>Plastic pollution is the most widespread problem affecting the marine environment. It also threatens ocean health, food safety and quality, human health, coastal tourism, and contributes to climate change. </p>
-          <img src={img1} style={{margin: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+          <p style={{textAlign: 'center',fontSize: 16,}}>The Great Pacific Garbage Patch is a collection of marine debris in the North Pacific Ocean. Also known as the Pacific trash vortex, the garbage patch is actually two distinct collections of debris bounded by the massive North Pacific Subtropical Gyre.</p>
+          <img src={plastic_patch} style={{marginLeft: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+          <img src={img1} style={{marginLeft: 22.5,marginTop: 10, width: '90%', height: 'auto', borderRadius: 10}}></img>
+          <p style={{textAlign: 'center',fontSize: 16,}}>MICROPLASTICS<br />Most debris in the Great Pacific Garbage Patch is plastic. Plastic is not biodegradable, meaning it does not disintegrate—it simply breaks into tinier and tinier pieces, known as microplastics. Microplastics of the Great Pacific Garbage Patch can simply make the water look like a cloudy soup.</p>
+          <img src={microplastics} style={{marginLeft: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+          <p style={{textAlign: 'center',fontSize: 16,}}>PLASTIC DEBRIS<br />Mixed in with microplastics are larger pieces of plastic. Most plastics are refuse from land activities in North American and Asia. Some plastics are accidentally dumped from oceangoing vessels. All the plastics on this page were culled from the Great Pacific Garbage Patch.</p>
+          <img src={plastic_debris} style={{marginLeft: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+          <p style={{textAlign: 'center',fontSize: 16,}}>The marine debris of the Great Pacific Garbage Patch can devastate marine life in the North Pacific Subtropical Gyre. The stomach contents of this unfortunate albatross include plastic marine debris fed to the chick by its parents.</p>
+          <img src={albatross} style={{marginLeft: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+          </div>
+          )}
+
+
+          {(page===2) && (
+            <div
+              style={{
+                background: 'rgba(0,0,0,0.6)',
+                fontFamily: 'Chelsea Market',
+                position: 'absolute',
+                fontSize: 20,
+                top: 70,
+                left: 20,
+                width: 450,
+                padding: 12,
+                color: 'white',
+                borderRadius: 20,
+                overflow: 'auto',
+                height: '90vh'
+              }}>
+              <button style={{
+                background: 'rgba(225,173,1,0.6)',
+                fontFamily: 'Chelsea Market',
+                fontSize: 20,
+                top: 0,
+                right: 20,
+                width: '100%',
+                padding: 12,
+                color: 'white',
+                borderRadius: 14,
+                border: 0,
+                position:'sticky',
+              }}>WHAT'S YOUR PLASTIC FOOTPRINT?</button> 
+              <br />
+              <p style={{textAlign: 'center',fontSize: 16,}}>Each year, an estimated 8 million tonnes of plastic end up in the ocean. Did you know that personal care products are a major source of microplastics? They get washed away into the oceans through our bathrooms.</p>
+              <img src={bathroom} style={{marginLeft: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+              <p style={{textAlign: 'center',fontSize: 16,}}>WHAT CAN YOU DO?<br />Heres a list of actions you can take to decrease your plastic footprint!</p>
+              <ul style={{fontSize: 16,}}>
+                <li>Switch to bamboo toothbrushes and straws</li>
+                <img src={bamboo} style={{margin: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+                <li>Try making your own toothpaste using baking soda, coconut oil and essential oils</li>
+                <li>Use a shampoo bar instead of shampoo bottles!</li>
+                <img src={shampoo_bars} style={{margin: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+                <li>Check the ingredients of beauty products for plastics, stop using products with glitter in, and switch to plastic free products. </li>
+                <li>Switch to reef-safe sunscreen which contains no plastic ingredients</li>
+                <img src={reef_safe} style={{margin: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+                <li>You can get a razor to last a lifetime by using a safety razor instead of disposable ones. </li>
+
+              </ul>
+              </div>
+              )}
+
+              {(page==3) && (
+                <div
+                  style={{
+                    background: 'rgba(0,0,0,0.6)',
+                    fontFamily: 'Chelsea Market',
+                    position: 'absolute',
+                    fontSize: 20,
+                    top: 70,
+                    left: 20,
+                    width: 450,
+                    padding: 12,
+                    color: 'white',
+                    borderRadius: 20,
+                    overflow: 'auto',
+                    height: '90vh'
+                  }}>
+                  <div style={{position:'sticky',}}>
+                  <button style={{
+                    background: 'rgba(0,100,0,0.6)',
+                    fontFamily: 'Chelsea Market',
+                    fontSize: 20,
+                    top: 0,
+                    right: 20,
+                    width: '100%',
+                    padding: 12,
+                    color: 'white',
+                    borderRadius: 14,
+                    border: 0,
+                    position:'sticky',
+                  }}>TAKE THE CLEAN SEAS PLEDGE</button> 
+                  </div>
+                  <br />
+                  <img src={clean_seas} style={{margin: 22.5, width: '90%', height: 'auto', borderRadius: 10}}></img>
+                  <button style={{
+                    background: 'rgba(0,0,100,0.6)',
+                    fontFamily: 'Chelsea Market',
+                    fontSize: 20,
+                    top: 0,
+                    right: 20,
+                    width: '100%',
+                    padding: 12,
+                    color: 'white',
+                    borderRadius: 14,
+                    border: 0,
+                    position:'sticky',
+                  }}>
+                  <a href="https://www.cleanseas.org/pledge" style={{ color: 'white', textDecoration: 'none'}}>MAKE A PLEDGE NOW!</a></button> <br/>
+                  <a href="https://www.nationalgeographic.org/encyclopedia/great-pacific-garbage-patch/" style={{ color: 'white', textDecoration: 'none', marginTop: 20, textAlign: 'center'}}><br/>Read more about the upcoming Plastic 'Pandemic' on National Geographic</a>
+                  <img src={nat_geo} style={{margin: 20, width: '90%', height: 'auto', background: 'white', padding: 10, borderRadius: 10}}></img>
+                  <a href="https://www.goplogging.org/welcome" style={{ color: 'white', textDecoration: 'none', marginTop: 20, textAlign: 'center'}}><br/>Start Plogging !</a>
+                  <img src={plogging} style={{margin: 20, width: '90%', height: 'auto', borderRadius: 10}}></img>
+                  <a href="https://www.nationalgeographic.org/encyclopedia/great-pacific-garbage-patch/" style={{ color: 'white', textDecoration: 'none', marginTop: 20, textAlign: 'center'}}><br/>Read about the OceanCleanup movement</a>
+                  <img src={ocean_cleanup} style={{margin: 20, width: '90%', height: 'auto', borderRadius: 10}}></img>
+                  </div>
+                  )}
+        
+
           {
           // <p>{details}</p>
           // <p>
@@ -136,10 +277,8 @@ function App() {
           //   {JSON.stringify(event.pointerEventPosition)})
           // </p>
         }
-        </div>
-        </>
-      )}
     </div>
+    </>
   );
 }
 
